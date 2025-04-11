8f36bbdd8a2dd4f802545f36fae2cb86054ff7d3
Kayla Hicks <kaylahicksa@gmail.com>
2025-04-11 15:14:58 -0700

    Create schemas.py

---
+++ src/app/schemas.py
@@ -0,0 +1,66 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class UserBase(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+class UserCreate(UserBase):
+    password: str
+class User(UserBase):
+    id: int
+    acc_created = datetime
+    class Config:
+        orm_mode = True
+
+class CalendarBase(BaseModel): 
+    display_type: str
+    name: str
+class CalendarCreate(CalendarBase): 
+    pass
+class Calendar(CalendarBase): 
+    id: int
+    class Config:
+        orm_mode = True
+
+class EventBase(BaseModel):
+    name: str
+    start_date: datetime
+    end_date: datetime
+    start_time: datetime.now
+    end_time: datetime.now
+    recurring: bool = False # default to False
+    description: str
+class EventCreate(EventBase):
+    pass
+class Event(EventBase):
+    id: int
+    created_at: datetime
+    class Config: 
+        orm_mode = True
+
+class HabitBase(BaseModel):
+    month: int = datetime.now().month  
+    day: int = datetime.now().day      
+    year: int = datetime.now().year    
+    name: str
+    duration: timedelta = 0                  
+    quantity: int = 0                  
+    category: str
+    description: str = None            
+    completed: bool = False            
+class HabitCreate(HabitBase):
+    pass  # Uses all fields from the base model
+class Habit(HabitBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class CategoryBase(BaseModel):
+    name: str
+class CategoryCreate(CategoryBase):
+    pass
+class Category(CategoryBase):
+    id: int
+    class Config: 
+        orm_mode = True
